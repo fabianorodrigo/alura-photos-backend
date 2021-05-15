c4ab9cdf439fd0d292a91115d932b81ef8132296
Fabiano Nascimento <fabianorodrigo@gmail.com>
2021-05-15 13:03:43 -0300

    Inclusão do HEADER x-access-token na resposta do login

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
+const accessTokenSecret = 'youraccesstokensecret';
 
 const app = express()
 app.use(cors())
@@ -9,11 +11,59 @@ const port = 3000
 
 app.post('/user/login/', (req, res) => {
     if(req.body.userName == 'fabiano' && req.body.password == 'senha'){
-      res.json({userName: req.body.userName, sucesso: true});
+      
+      const accessToken = jwt.sign({userName: req.body.userName, roles: ['FULL']}, accessTokenSecret);
+      res.header('x-access-token', accessToken).json({userName: req.body.userName, sucesso: true});
     }else{
-        res.status(401).send(`Usuário senha inválidos`);
+        res.status(401).send(`Usuário senha inválidos: ${req.body.userName}, ${req.body.password}`);
     }
-})
+});
+
+app.get('/:userName/fotos', (req, res) => {
+  let photos = [];
+  if (req.params.userName === 'fabiano') {
+    photos = [
+      {
+        url: 'https://uploads-ssl.webflow.com/5fa55b2822303ed3e04a22da/5fb5364bf8849937693160db_27.png',
+        description: 'Ancine',
+      },
+      {
+        url: 'http://dsgov.estaleiro.serpro.gov.br/assets/img/govbr-logo-large.png',
+        description: 'govbr',
+      },
+      {
+        url: 'https://cdn.sstatic.net/Img/teams/teams-illo-free-sidebar-promo.svg?v=47faa659a05e',
+        description: 'Stack',
+      },
+      {
+        url: 'https://assets.umbler.com/site/home/2021/header-app-dashboard.png?v=20210428030328',
+        description: 'Umbler',
+      },
+      {
+        url: 'https://pbs.twimg.com/card_img/1387120330481754118/YgXB4tsx?format=jpg&name=small',
+        description: 'Prettier',
+      },
+    ];
+  } else {
+    photos = [
+      {
+        url:
+          'https://s2.glbimg.com/x8GqURWkuqSUW7G56YxUVlFWL5U=/0x0:1700x1065/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/j/Y/uArM7YRvCyVOA7DvSReA/china1.jpg',
+        description: 'Foguete',
+      },
+      {
+        url:
+          'https://s2.glbimg.com/LMLg9k_ETyaiJkUNpYAOaBIDZ1Q=/85x47:1648x927/540x304/smart/filters:max_age(3600)/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2021/6/s/JJicXXQ2O7P9GOwjD40A/ez3vlz-wyaeaasc.jpg',
+        description: 'Galeano',
+      },
+      {
+        url: 'https://assets.umbler.com/site/home/2021/header-app-dashboard.png?v=20210428030328',
+        description: 'Umbler',
+      },
+    ];
+  }
+  res.json(photos);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
